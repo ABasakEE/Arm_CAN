24c62303cd9a5444e2ef15c13888147ae25ba47c
ABasakEE <arjoe.basak@gmail.com>
2024-12-20 22:46:50 +0530

    Minor changes
    Modified code to ensure shoulder and elbow encoders increase in the same direction

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -356,9 +356,9 @@ static void MX_TIM4_Init(void)
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 0;
   if (SLAVE_ID-SLAVE0_ID == 1) //shoulder node needs to count down
-	  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  else
 	  htim4.Init.CounterMode = TIM_COUNTERMODE_DOWN;
+  else
+	  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 65535;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
